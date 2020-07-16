f28f086284fc1127bc85bb56431fd369cab42db5
Joanna Preston <joannappreston@gmail.com>
2020-07-16 16:35:15 -0700

    Updates

---
+++ precache-manifest.d9a9b7da29e58d621a79a3ea66d3a2f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7c024b84259df13cff410dfe92f6e3",
+    "url": "/React-Pupster/index.html"
+  },
+  {
+    "revision": "db644b7fa67fdc8f658f",
+    "url": "/React-Pupster/static/css/main.9b6f1185.chunk.css"
+  },
+  {
+    "revision": "a5fb92cc0798ee4233a3",
+    "url": "/React-Pupster/static/js/2.005058fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Pupster/static/js/2.005058fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db644b7fa67fdc8f658f",
+    "url": "/React-Pupster/static/js/main.3cceaed3.chunk.js"
+  },
+  {
+    "revision": "61fa611bfaf80cf95c80",
+    "url": "/React-Pupster/static/js/runtime-main.0e222567.js"
+  }
+]);
